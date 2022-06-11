bd55adcf150510a5f47cc7a7d8aee4b5466ca6ce
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-06-11 16:39:25 +0400

    Build tree

--- products_analyzer/shop_unit/helpers.py
+++ products_analyzer/shop_unit/helpers.py
@@ -1,7 +1,9 @@
 import uuid
 
 from shop_unit.models import ShopUnit
+from shop_unit.serializers import ShopUnitSerializer
 from shop_unit.types import ShopUnitTypes
+from datetime import datetime
 
 
 def calculate_category_price(category: ShopUnit) -> int | None:
@@ -43,9 +45,45 @@ def remove_timezone_suffix(date: str) -> str:
     return date
 
 
+def format_date(date):
+    if date.endswith('Z'):
+        date = date.replace('Z', '.000Z')
+    return date
+
+
 def is_valid_uuid(value: str) -> bool:
     try:
         uuid.UUID(str(value))
         return True
     except ValueError:
         return False
+
+
+def format_shop_unit_data(shop_unit: ShopUnit, data):
+    data['date'] = format_date(data['date'])
+    data['parentId'] = None
+    if shop_unit.parent:
+        data['parentId'] = str(shop_unit.parent.id)
+
+    del data['parent']
+    return data
+
+
+def get_cleaned_data(shop_unit, request):
+    data = ShopUnitSerializer(shop_unit, context={'request': request}).data
+    return format_shop_unit_data(shop_unit, data)
+
+
+def build_tree(shop_unit: ShopUnit, request, tree=None):
+    if tree is None:
+        tree = []
+    for child in shop_unit.children.all():
+        if child.type == ShopUnitTypes.CATEGORY.name:
+            data = get_cleaned_data(child, request)
+            tree.append(data)
+            tree[-1]['children'] = build_tree(child, request, [])
+        else:
+            data = get_cleaned_data(child, request)
+            data['children'] = None
+            tree.append(data)
+    return tree
