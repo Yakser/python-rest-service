46ca791c648667a7872cae292f59ffb74ae9111d
Sergey Yaksanov <iaksanov2044@mail.ru>
2022-06-13 17:35:37 +0400

    Type hints

--- products_analyzer/shop_unit/helpers.py
+++ products_analyzer/shop_unit/helpers.py
@@ -3,6 +3,7 @@ import uuid
 from shop_unit.models import ShopUnit
 from shop_unit.serializers import ShopUnitSerializer
 from shop_unit.types import ShopUnitTypes
+from rest_framework.request import Request
 
 
 def remove_timezone_suffix(date: str) -> str:
@@ -11,7 +12,7 @@ def remove_timezone_suffix(date: str) -> str:
     return date
 
 
-def format_date(date):
+def format_date(date: str) -> str:
     if date.endswith('Z'):
         date = date.replace('Z', '.000Z')
     return date
@@ -25,24 +26,27 @@ def is_valid_uuid(value: str) -> bool:
         return False
 
 
-def format_shop_unit_data(shop_unit: ShopUnit, data):
+def format_shop_unit_data(shop_unit: ShopUnit, data: dict) -> dict:
     data['date'] = format_date(data['date'])
     data['parentId'] = None
+
     if shop_unit.parent:
         data['parentId'] = str(shop_unit.parent.id)
 
     del data['parent']
+
     return data
 
 
-def get_cleaned_data(shop_unit, request):
+def get_cleaned_data(shop_unit: ShopUnit, request: Request) -> dict:
     data = ShopUnitSerializer(shop_unit, context={'request': request}).data
     return format_shop_unit_data(shop_unit, data)
 
 
-def build_tree(shop_unit: ShopUnit, request, tree=None):
+def build_tree(shop_unit: ShopUnit, request: Request, tree=None) -> list[dict]:
     if tree is None:
         tree = []
+
     for child in shop_unit.children.all():
         if child.type == ShopUnitTypes.CATEGORY.name:
             data = get_cleaned_data(child, request)
@@ -51,5 +55,7 @@ def build_tree(shop_unit: ShopUnit, request, tree=None):
         else:
             data = get_cleaned_data(child, request)
             data['children'] = None
+
             tree.append(data)
+
     return tree
